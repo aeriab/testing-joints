946052161d90d0266efbe7898a07fd844e770022
Brendan Aeria <89553639+aeriab@users.noreply.github.com>
2026-02-22 20:24:59 -0800

    Very cool, but pressure in the middle adds up

--- spawn_rigid_bodies.gd
+++ spawn_rigid_bodies.gd
@@ -5,7 +5,7 @@ extends Node2D
 var stone: Stone
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("spawn"):
+	if Input.is_action_pressed("spawn"):
 		stone = stone_scene.instantiate() as Stone
 		stone.global_position = get_global_mouse_position()
 		get_parent().add_child(stone)
